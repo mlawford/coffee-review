3b9ad1ff90dcf8ba1330818576ce2cfb140b9b5b
ndean87 <ndean87@gmail.com>
2017-10-25 09:27:59 -0400

    markers with infowindows working

--- src/index.js
+++ src/index.js
@@ -1,37 +1,47 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  console.log("DOM fully loaded and parsed");
-
-  initMap()
-
   const reviewList = document.getElementById('review-list')
-
-  fetch('http://localhost:3000/api/v1/reviews')
-
-    .then(res => res.json())
-
-    .then(json => json.forEach(review => {
-       reviewList.appendChild(new Review(review).renderReview())
-  })
-)
-
-
-
-// var marker = new google.maps.Marker({
-//    position: myLatLng,
-//    map: map,
-//    title: 'Hello World!'
-//  });
-    // function appendReview(review){
-    //   const reviewHolder = document.createElement('li')
-    //   reviewHolder.innerText = review.name
-    //   reviewList.appendChild(reviewHolder)
-    // }
-});
-
-function initMap() {
+  var geocoder = new google.maps.Geocoder();
+  var infowindow = new google.maps.InfoWindow();
   var school = {lat: 40.705439, lng: -74.014002};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
+    zoom: 11,
     center: school
   });
-}
+
+  fetch('http://localhost:3000/api/v1/reviews')
+  .then(res => res.json())
+  .then(json => json.forEach(review => {
+    console.log('review', review);
+    reviewList.appendChild(new Review(review).renderReview())
+
+    geocoder.geocode( { 'address': review.address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        results.forEach((result, i) => {
+          const lat = result.geometry.location.lat();
+          const lng = result.geometry.location.lng();
+          console.log({lat, lng});
+          var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(lat, lng),
+            map: map,
+          });
+
+          google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+              infowindow.setContent(`
+                <h2>${review.name}</h2>
+                <div>Rating: ${review.rating}stars</div>
+                <div>What Did You Order?: ${review.drink_ordered}</div>
+                <div>How Much Was It?: $${review.price}</div>
+                `);
+              infowindow.open(map, marker);
+            }
+          })(marker, i));
+        })
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }))
+  .catch(e => console.log(e))
+
+});
